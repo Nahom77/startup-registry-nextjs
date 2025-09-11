1da1daab5d994cc30b7cf853e073bd2d7f2c2864
Nahom77 <nahomtigistu777@gmail.com>
2025-09-11 19:30:31 +0300

    User registered in sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,8 @@
-import StartupCard, {StartupCardType} from '@/components/StartupCard';
-import SearchForm from '../../components/SearchForm';
-import { client } from '@/sanity/lib/client';
-import { STARTUPS_QUERY } from '@/sanity/lib/queries';
-
-
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { auth } from "@/auth";
 
 export default async function Home({
   searchParams,
@@ -11,34 +10,39 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  const params = {search: query || null}
+  const params = { search: query || null };
+
+  const session = await auth();
+  console.log(session?.id);
 
   const posts = await client.fetch(STARTUPS_QUERY, params);
 
   return (
     <>
       {/* Hero section */}
-      <section className='pink_container'>
-        <h1 className='heading'>
+      <section className="pink_container">
+        <h1 className="heading">
           Pitch Your Startup, <br /> Connect With Entrepreneurs
         </h1>
 
-        <p className='!max-w-3xl sub-heading'>
+        <p className="!max-w-3xl sub-heading">
           Submit Ideas, and Get Noticed in Virtual Competitions.
         </p>
 
         <SearchForm query={query} />
       </section>
 
-      <section className='section_container'>
-        <p className='text-30-semibold'>
-          {query ? `Search results for ${query}` : 'All Startups'}
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
         </p>
 
-        <ul className='mt-7 card_grid'>
+        <ul className="mt-7 card_grid">
           {posts?.length > 0
-            ? posts.map((post: StartupCardType) => <StartupCard key={post?._id} post={post} />)
-            : ''}
+            ? posts.map((post: StartupCardType) => (
+                <StartupCard key={post?._id} post={post} />
+              ))
+            : ""}
         </ul>
       </section>
     </>
