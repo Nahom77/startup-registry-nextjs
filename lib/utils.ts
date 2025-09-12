ddd700c857e169f8aebb00539227e3ee05359358
Nahom77 <nahomtigistu777@gmail.com>
2025-09-12 06:19:57 +0300

    Startup form created, validation schema created and /create endpoint created

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,14 +1,18 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function formatDate(date: string) {
-  return new Date(date).toLocaleDateString('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
   });
 }
+
+// "predev": "npm run typegen",
+// "prebuild": "npm run typegen",
+// "typegen": "sanity schema extract --path=./sanity/extract.json && sanity typegen generate"
